ce1e2d2aab203e7eb642e29257bb553eed4434eb
Nick <nick@nicholas-morgan.com>
2022-06-03 17:50:21 -0700

    Deploying to `master` on Fri Jun  3 17:50:18 PDT 2022.

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,31 @@ CURRENT_GIT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
 echo "The Current Git Branch Is: \`${CURRENT_GIT_BRANCH}\`";
 echo "";
 
-GIT_TAG_VERSION=$(git describe --tags --abbrev=0)
-read -p "Rebuild \`./dist/${GIT_TAG_VERSION}.tar.gz\`? (Y/N): " TO_REBUILD_DISTRIBUTION
+# GIT_TAG_VERSION=$(git describe --tags --abbrev=0)
+# read -p "Rebuild \`./dist/${GIT_TAG_VERSION}.tar.gz\`? (Y/N): " TO_REBUILD_DISTRIBUTION
 
-if [ "${TO_REBUILD_DISTRIBUTION}" == "Y" ] || [ "${TO_REBUILD_DISTRIBUTION}" == "y" ]; then
-	echo "Rebuilding \`./dist/${GIT_TAG_VERSION}\`...";
-	./build.sh
-    echo "\`./dist/${GIT_TAG_VERSION}\` Rebuilt!";
+# if [ "${TO_REBUILD_DISTRIBUTION}" == "Y" ] || [ "${TO_REBUILD_DISTRIBUTION}" == "y" ]; then
+# 	echo "Rebuilding \`./dist/${GIT_TAG_VERSION}\`...";
+# 	./build.sh
+#     echo "\`./dist/${GIT_TAG_VERSION}\` Rebuilt!";
+# else
+# 	echo "Did Not Rebuild.";
+# fi
+# echo "";
+
+
+
+read -p "Rebuild \`./dist/${GIT_TAG_VERSION}.tar.gz\`? (Y/N) (Y): " TO_RUN_REBUILD
+if [ "${TO_RUN_REBUILD}" == "" ] || [ "${TO_RUN_REBUILD}" == "Y" ] || [ "${TO_RUN_REBUILD}" == "y" ]; then
+	# git add -A .
+	# echo -e "Git Command Executed!: \t \`git add -A .\` ";
+	# echo "";
+	echo -e "✅ Rebuilding";
+	TO_RUN_REBUILD="Y"
 else
-	echo "Did Not Rebuild.";
+	TO_RUN_REBUILD="N"
 fi
 echo "";
-
-# git_add_commit.sh
-
-
-
 
 
 
@@ -29,18 +38,20 @@ if [ "${TO_RUN_GIT_ADD}" == "" ] || [ "${TO_RUN_GIT_ADD}" == "Y" ] || [ "${TO_RU
 	# git add -A .
 	# echo -e "Git Command Executed!: \t \`git add -A .\` ";
 	# echo "";
+	echo -e "✅ Including Git Add";
 	TO_RUN_GIT_ADD="Y"
 else
 	TO_RUN_GIT_ADD="N"
 fi
+echo "";
 
 
-read -p "Run \`git commit -m <message>\`? (Y/N): " TO_RUN_GIT_COMMIT
+read -p "Run \`git commit -m <message>\`? (Y/N) (Y): " TO_RUN_GIT_COMMIT
 if [ "${TO_RUN_GIT_COMMIT}" == "" ] || [ "${TO_RUN_GIT_COMMIT}" == "Y" ] || [ "${TO_RUN_GIT_COMMIT}" == "y" ]; then
 
 	DEFAULT_GIT_COMMIT_MESSAGE="Deploying to \`${CURRENT_GIT_BRANCH}\` on `date`."
 
-	read -p "Enter Custom Git Commit Message (\"${DEFAULT_GIT_COMMIT_MESSAGE}\"): " GIT_COMMIT_MESSAGE
+	read -p "Enter Git Commit Message (\"${DEFAULT_GIT_COMMIT_MESSAGE}\"): " GIT_COMMIT_MESSAGE
 	# Check if a git commit message wasn't entered, and use a default one if so.
 	if [[ -z "${GIT_COMMIT_MESSAGE}" ]]; then
 	   GIT_COMMIT_MESSAGE="${DEFAULT_GIT_COMMIT_MESSAGE}";
@@ -49,63 +60,95 @@ if [ "${TO_RUN_GIT_COMMIT}" == "" ] || [ "${TO_RUN_GIT_COMMIT}" == "Y" ] || [ "$
 	# git commit -m "${GIT_COMMIT_MESSAGE}";
 	# echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
 	# echo "";
+	echo -e "✅ Including Git Commit \n\t Message: ${GIT_COMMIT_MESSAGE}";
 	TO_RUN_GIT_COMMIT="Y"
 else
 	TO_RUN_GIT_COMMIT="N"
 fi
+echo "";
 
 
 
 
-read -p "Run \`git push\`? (Y/N): " TO_RUN_GIT_PUSH
+read -p "Run \`git push\`? (Y/N) (Y): " TO_RUN_GIT_PUSH
 if [ "${TO_RUN_GIT_PUSH}" == "" ] || [ "${TO_RUN_GIT_PUSH}" == "Y" ] || [ "${TO_RUN_GIT_PUSH}" == "y" ]; then
 	# git push
 	# echo -e "Git Command Executed!: \t \`git push\` ";
 	# echo "";
+	echo -e "✅ Including Git Push";
 	TO_RUN_GIT_PUSH="Y"
 else
 	TO_RUN_GIT_PUSH="N"
 fi
-
-
-
-
-
-if [ "${TO_RUN_GIT_ADD}" == "Y" ]; then
-	git add -A .
-	echo -e "Git Command Executed!: \t \`git add -A .\` ";
-	echo "";
-fi
-
-if [ "${TO_RUN_GIT_COMMIT}" == "Y" ]; then
-	git commit -m "${GIT_COMMIT_MESSAGE}";
-	echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
-	echo "";
-fi
-
-if [ "${TO_RUN_GIT_PUSH}" == "Y" ]; then
-	git push
-	echo -e "Git Command Executed!: \t \`git push\` ";
-	echo "";
-fi
-
-
-# read -p " \`git add -A .\` , \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` , \`git push\`   Look good? (Y/N): " TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH
-# if [ "${TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH}" == "Y" ] || [ "${TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH}" == "y" ]; then
-# 	git add -A .
-# 	git commit -m "${GIT_COMMIT_MESSAGE}";
-# 	git push
-# else
-# 	TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH="N"
-# fi
-
-
-
-
-echo "Deployment Finished!";
-echo \e "Summary: \n\t Rebuilt: ${TO_REBUILD_DISTRIBUTION} \n\t Git Add: ${TO_RUN_GIT_ADD} \n\t Git Commit: ${TO_RUN_GIT_COMMIT} \n\t Git Push: ${TO_RUN_GIT_PUSH}":
 echo "";
-exit 0;
+
+
+
+
+
+
+
+
+
+
+
+#
+# First confirm the deployment with the user (ask for input).
+#
+echo -e "ℹ️  Deployment Summary: \n\t Rebuilt: ${TO_RUN_REBUILD} \n\t Git Add: ${TO_RUN_GIT_ADD} \n\t Git Commit: ${TO_RUN_GIT_COMMIT} \n\t Git Push: ${TO_RUN_GIT_PUSH}":
+echo "";
+read -p "⚠️  Confirm Deployment? (Y/N) (N): " THE_DEPLOYMENT_IS_CONFIRMED
+if [ "${THE_DEPLOYMENT_IS_CONFIRMED}" == "Y" ] || [ "${THE_DEPLOYMENT_IS_CONFIRMED}" == "y" ]; then
+	THE_DEPLOYMENT_IS_CONFIRMED="Y"
+else
+	THE_DEPLOYMENT_IS_CONFIRMED="N"
+fi
+
+
+
+
+#
+# Finally, actually deploy or cancel.
+#
+if [ "${THE_DEPLOYMENT_IS_CONFIRMED}" == "Y" ]; then
+
+	if [ "${TO_RUN_REBUILD}" == "Y" ]; then
+		echo "Rebuilding \`./dist/${GIT_TAG_VERSION}\`...";
+		./build.sh
+		echo "\`./dist/${GIT_TAG_VERSION}\` Rebuilt!";
+	fi
+
+	if [ "${TO_RUN_GIT_ADD}" == "Y" ]; then
+		git add -A .
+		echo -e "Git Command Executed!: \t \`git add -A .\` ";
+		echo "";
+	fi
+
+	if [ "${TO_RUN_GIT_COMMIT}" == "Y" ]; then
+		git commit -m "${GIT_COMMIT_MESSAGE}";
+		echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
+		echo "";
+	fi
+
+	if [ "${TO_RUN_GIT_PUSH}" == "Y" ]; then
+		git push
+		echo -e "Git Command Executed!: \t \`git push\` ";
+		echo "";
+	fi
+
+	echo "✅ Deployment Succeeded!";
+	echo "";
+	exit 0;
+
+else
+	echo "🚫 Deployment Canceled!";
+	echo "";
+	exit 1;
+fi
+
+
+
+
 
 
 # echo "${GIT_COMMIT_MESSAGE}"
