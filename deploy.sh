b25177ec212e88954e1c6813c1c4a5b2e6083773
Nick <nick@nicholas-morgan.com>
2022-06-03 17:31:18 -0700

    Deploying to `master` on Fri Jun  3 17:31:13 PDT 2022.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Deployment...";
 CURRENT_GIT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
-echo "Git Branch: \`${CURRENT_GIT_BRANCH}\`";
+echo "The Current Git Branch Is: \`${CURRENT_GIT_BRANCH}\`";
 echo "";
 
 GIT_TAG_VERSION=$(git describe --tags --abbrev=0)
@@ -24,17 +24,19 @@ echo "";
 
 
 
-read -p "Run \`git add -A .\`? (Y/N): " TO_RUN_GIT_ADD
-if [ "${TO_RUN_GIT_ADD}" == "Y" ] || [ "${TO_RUN_GIT_ADD}" == "y" ]; then
-	git add -A .
-
-	echo -e "Git Command Executed!: \t \`git add -A .\` ";
-	echo "";
+read -p "Run \`git add -A .\`? (Y/N) (Y): " TO_RUN_GIT_ADD
+if [ "${TO_RUN_GIT_ADD}" == "" ] || [ "${TO_RUN_GIT_ADD}" == "Y" ] || [ "${TO_RUN_GIT_ADD}" == "y" ]; then
+	# git add -A .
+	# echo -e "Git Command Executed!: \t \`git add -A .\` ";
+	# echo "";
+	TO_RUN_GIT_ADD="Y"
+else
+	TO_RUN_GIT_ADD="N"
 fi
 
 
 read -p "Run \`git commit -m <message>\`? (Y/N): " TO_RUN_GIT_COMMIT
-if [ "${TO_RUN_GIT_COMMIT}" == "Y" ] || [ "${TO_RUN_GIT_COMMIT}" == "y" ]; then
+if [ "${TO_RUN_GIT_COMMIT}" == "" ] || [ "${TO_RUN_GIT_COMMIT}" == "Y" ] || [ "${TO_RUN_GIT_COMMIT}" == "y" ]; then
 
 	DEFAULT_GIT_COMMIT_MESSAGE="Deploying to \`${CURRENT_GIT_BRANCH}\` on `date`."
 
@@ -44,21 +46,60 @@ if [ "${TO_RUN_GIT_COMMIT}" == "Y" ] || [ "${TO_RUN_GIT_COMMIT}" == "y" ]; then
 	   GIT_COMMIT_MESSAGE="${DEFAULT_GIT_COMMIT_MESSAGE}";
 	fi
 
-	git commit -m "${GIT_COMMIT_MESSAGE}";
-
-	echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
-	echo "";
+	# git commit -m "${GIT_COMMIT_MESSAGE}";
+	# echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
+	# echo "";
+	TO_RUN_GIT_COMMIT="Y"
+else
+	TO_RUN_GIT_COMMIT="N"
 fi
+
 
 
 
 read -p "Run \`git push\`? (Y/N): " TO_RUN_GIT_PUSH
-if [ "${TO_RUN_GIT_PUSH}" == "Y" ] || [ "${TO_RUN_GIT_PUSH}" == "y" ]; then
-	git push
+if [ "${TO_RUN_GIT_PUSH}" == "" ] || [ "${TO_RUN_GIT_PUSH}" == "Y" ] || [ "${TO_RUN_GIT_PUSH}" == "y" ]; then
+	# git push
+	# echo -e "Git Command Executed!: \t \`git push\` ";
+	# echo "";
+	TO_RUN_GIT_PUSH="Y"
+else
+	TO_RUN_GIT_PUSH="N"
+fi
 
+
+
+
+
+if [ "${TO_RUN_GIT_ADD}" == "Y" ]; then
+	git add -A .
+	echo -e "Git Command Executed!: \t \`git add -A .\` ";
+	echo "";
+fi
+
+if [ "${TO_RUN_GIT_COMMIT}" == "Y" ]; then
+	git commit -m "${GIT_COMMIT_MESSAGE}";
+	echo -e "Git Command Executed!: \t \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` ";
+	echo "";
+fi
+
+if [ "${TO_RUN_GIT_PUSH}" == "Y" ]; then
+	git push
 	echo -e "Git Command Executed!: \t \`git push\` ";
 	echo "";
 fi
+
+
+# read -p " \`git add -A .\` , \`git commit -m \"${GIT_COMMIT_MESSAGE}\"\` , \`git push\`   Look good? (Y/N): " TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH
+# if [ "${TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH}" == "Y" ] || [ "${TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH}" == "y" ]; then
+# 	git add -A .
+# 	git commit -m "${GIT_COMMIT_MESSAGE}";
+# 	git push
+# else
+# 	TO_EXECUTE_GIT_ADD_COMMIT_AND_PUSH="N"
+# fi
+
+
 
 
 echo "Deployment Finished!";
